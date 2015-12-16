2f68107a66b988632fc5ab7fa9b3c78e4ebb0f0a
Andrew Persad <akpersad@gmail.com>
2015-12-16 10:08:18 -0500

    done

--- lib/unique_uniq_method.rb
+++ lib/unique_uniq_method.rb
@@ -1,5 +1,15 @@
+require 'pry'
 class Array
   def unique_uniq
-    # code goes here
+    arr = []
+    arr << self.shift
+    self.each do |self_item|
+    	arr.each do |arr_item|
+    		if !arr.include?(self_item)
+    			arr << self_item
+    		end
+    	end
+    end
+	arr
   end
 end
